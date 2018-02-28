cc73263495e1e6be3b5d9b960b215226170df7d7
Cort Haldaman <cort.haldaman@gingerbreadshed.com>
2018-02-28 14:09:45 -0700

    customizing for shed use.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Nginx version
-VERSION=1.13.0
+VERSION=1.13.9
 
-TAG="netkuy/nginx:$VERSION"
+# TAG="netkuy/nginx-resolver:$VERSION"
 
-docker build -t "$TAG" --build-arg VERSION="$VERSION" .
-docker push "$TAG"
+# docker build -t "$TAG" --build-arg VERSION="$VERSION" .
+# docker push "$TAG"
